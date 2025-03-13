5cda75c31bed4fc561f5ade54d073231eead56a1
Fabrice Bokovi <Fabrice.Bokovi@virtualincision.com>
2025-03-12 18:18:24 -0700

    added comments to timeConversion

--- timeConversion.py
+++ timeConversion.py
@@ -6,32 +6,32 @@ import sys
 
 def timeConversion(s):
     # Write your code here
-    
     time_suffix = s[8:len(s)]
     hour = int(s[0:2])
     minute = s[3:5]
     seconds = s[6:8]
     
     if time_suffix == 'PM':
+        # 12PM edge case
         if hour == 12:
             return f"{hour:02}:{minute}:{seconds}"
-            
+        
+        # conversion
         military_hour = 12 + hour
         return f"{military_hour}:{minute}:{seconds}"        
+    
     elif time_suffix == 'AM':
+        # midnight edge case
         if hour == 12:
             military_hour = "00"
             return f"{military_hour}:{minute}:{seconds}"
-        
+    
+    # if all else fails, it's AM and no conversion is needed
     return f"{hour:02}:{minute}:{seconds}"
 
 if __name__ == '__main__':
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = input()
 
     result = timeConversion(s)
     print(result)
-    # fptr.write(result + '\n')
-
-    # fptr.close()
